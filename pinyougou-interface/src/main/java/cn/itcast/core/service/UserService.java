86cc2258652b1447fe83e947d94ea9b7e0df9488
sun <sun@163.com>
2019-05-12 09:52:21 +0800

    1：pinyougou-dao模块中orderItemDao接口中添加分组查询方法，mapper文件中对应映射sql 2：pinyougou-interface模块中新增OrderitemService接口定义方法：findPic(),findPic2()用于折线图饼图的相应数据  的查询   新增PayService接口定义方法closePay(),用于关闭微信支付接口   UserService接口中定义方法search(),findTotal(),用于运营商后台统计 3：pinyougou-pojo模块中新增OrderGroup类型pojo，用于封装分组查询结果
    Signed-off-by: sun <sun@163.com>

--- pinyougou-interface/src/main/java/cn/itcast/core/service/UserService.java
+++ pinyougou-interface/src/main/java/cn/itcast/core/service/UserService.java
@@ -1,10 +1,20 @@
 package cn.itcast.core.service;
 
-import cn.itcast.core.pojo.user.User; /**
+import cn.itcast.core.pojo.user.User;
+import entity.PageResult;
+
+import java.util.List;
+
+/**
  * Created by wang on 2019/4/26.
  */
 public interface UserService {
     void sendCode(String phone);
 
     void add(String smscode, User user);
+
+    PageResult search(Integer pageNum, Integer pageSize, User user);
+
+    Integer findTotal();
+
 }
